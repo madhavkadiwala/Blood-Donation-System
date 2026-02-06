5c9d6910b24bdf30a9b73ba0a0aca4efee5db963
madhavkadiwala <22bmiit056@gmail.com>
2026-02-07 02:43:56 +0530

    Update blood_donation_system.sql

--- blood_donation_system.sql
+++ blood_donation_system.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 26, 2025 at 08:08 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
